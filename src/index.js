53381da28eec5a11e61b13f341e476fcfd59eb68
Luan Vinicius <nalucode@gmail.com>
2023-02-10 15:03:46 -0300

    Atualização e criação de novas tasks.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors")
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors())
 
 const tasks = [
   { id: 1, task: "Fazer compras" },
@@ -11,6 +16,43 @@ const tasks = [
 app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
+
+app.post("/tasks", (req, res) => {
+  const body = req.body
+  const lastTaskId = tasks[tasks.length - 1].id 
+
+  console.log(body)
+
+  if (body.task) {    
+    tasks.push({ id: lastTaskId + 1, task: body.task })
+
+    return res.status(201).send('Task created.')
+
+  } else {
+    return res.status(400).send('Malformed body')
+  }
+})
+
+app.put("/tasks/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+  const body = req.body
+
+  if (id && body.task != '') {
+    const taskIndex = tasks.findIndex(t => t.id === id)
+
+    if (taskIndex >= 0) {
+      tasks[taskIndex] = { id, task: body.task}
+
+      return res.status(200).send('Task updated.')
+
+    } else {
+      return res.status(404).send('Task not found.')
+    }
+
+  } else {
+    return res.status(400).send('Malformed body.')
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
